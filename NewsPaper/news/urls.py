acace8e058ab636472845b403d448ef09c0a7aa8
fotolx <63285940+fotolx@users.noreply.github.com>
2023-02-16 22:04:02 +0300

    Added post creation.

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PostsList, PostDetail, SearchList
+from .views import PostsList, PostDetail, SearchList, PostsAdd, PostEdit, PostDelete
  
  
 urlpatterns = [
     path('', PostsList.as_view()), 
     path('<int:pk>', PostDetail.as_view()),
-    path('search', SearchList.as_view()), 
+    path('search', SearchList.as_view()),
+    path('add', PostsAdd.as_view()), 
+    path('<int:pk>/edit/', PostEdit.as_view()),
+    path('<int:pk>/delete/', PostDelete.as_view()),
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -12,6 +12,16 @@ class PostsList(ListView):
     queryset = Post.objects.order_by('-id')
     paginate_by = 10
 
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        # context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset()) # вписываем наш фильтр в контекст
+        context['categories'] = Category.objects.all()
+        context['authors'] = Author.objects.all()
+        context['types'] = Post.TYPES
+        return context
+
+    
+
 class PostDetail(DetailView):
     model = Post
     template_name = 'post.html'
@@ -29,3 +39,45 @@ class SearchList(ListView):
         context = super().get_context_data(**kwargs)
         context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset()) # вписываем наш фильтр в контекст
         return context
+    
+class PostsAdd(ListView):
+    model = Post
+    template_name = 'posts_add.html'
+    context_object_name = 'posts'
+    queryset = Post.objects.order_by('-id')
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        # context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset()) # вписываем наш фильтр в контекст
+        context['categories'] = Category.objects.all()
+        context['authors'] = Author.objects.all()
+        context['types'] = Post.TYPES
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        # берём значения для нового товара из POST-запроса отправленного на сервер
+        author = request.POST['authors']
+        type = request.POST['type']
+        header = request.POST['header']
+        main_text = request.POST['main_text']
+        rating = request.POST['rating']
+        category = request.POST['category']
+        post = Post(author=Author.objects.get(username=author), type=type, header=header, 
+        main_text=main_text, rating=rating)   # создаём новую публикацию
+        post.save()   # и сохраняем
+        post.category.set(category) # Добавляем категорию
+        post.save()                                                          # и сохраняем
+        return super().get(request, *args, **kwargs) # отправляем пользователя обратно на GET-запрос.
+
+class PostEdit(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+    pass
+
+class PostDelete(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+    pass
